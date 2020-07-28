c52cbbb60010ae24127bc3085dbca2a91f834ae5
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-28 20:22:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ end
 def remove_element_from_end_of_array(array)
   array = ["wow", "I", "am", "really", "learning"]
  element = "arrays!"
- array
+ array.pop
 end
 
 def remove_element_from_start_of_array(array)
